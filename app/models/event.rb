649771451f8ffa5fdcb93b2e8dcdd064446ca090
Giancarlo <gpbene@gmail.com>
2019-08-12 11:54:37 +0200

    simpler datetime verification

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,8 @@ class Event < ApplicationRecord
     presence: true,
     length: { in: 5..140 }
   validates :start_date,
-    presence: true
+    presence: true,
+    numericality: {greater_than: DateTime.now.to_i}
   validates :duration,
     presence: true,
     numericality: { greater_than: 0 }
@@ -13,22 +14,15 @@ class Event < ApplicationRecord
     length: { in: 20..1000 }
   validates :price,
     presence: true,
-    numericality: { greater_than: 1 },
+    numericality: { greater_than: 0 },
     numericality: { less_than: 1000 }
   validates :location,
     presence: true
 
   validate :duration_must_be_multiple_of_five
-  validate :start_date_cannot_be_in_the_past
 
   has_many :attendances
   has_many :users, through: :attendances
-
-  def start_date_cannot_be_in_the_past
-    if start_date.present? && start_date < DateTime.now
-      errors.add(:start_date, "Start date can't be in the past")
-    end
-  end  
 
   def duration_must_be_multiple_of_five
     if duration % 5 != 0
