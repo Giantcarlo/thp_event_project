a59bd1b2e141713ee5264c6650178ee5080e8917
Giancarlo <gpbene@gmail.com>
2019-08-13 17:18:05 +0200

    starting event creation form

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,20 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root "event#index"
+  root "events#index"
   resources :events
   resources :users
 
