511d9e8f6e029b484e54d944d6df16aeb6fd1a24
Giancarlo <gpbene@gmail.com>
2019-08-16 11:07:16 +0200

    remove template, add admin to user, add button in dropdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,19 +16,3 @@
 //= require_tree .
 
 
-//= require assets/js/jquery.min
-//= require assets/js/jquery-migrate-3.0.1.min
-//= require assets/js/popper.min
-//= require assets/js/bootstrap.min
-//= require assets/js/jquery.easing.1.3
-//= require assets/js/jquery.waypoints.min
-//= require assets/js/jquery.stellar.min
-//= require assets/js/owl.carousel.min
-//= require assets/js/jquery.magnific-popup.min
-//= require assets/js/aos
-//= require assets/js/jquery.animateNumber.min
-//= require assets/js/bootstrap-datepicker
-//= require assets/js/jquery.timepicker.min
-//= require assets/js/scrollax.min
-//= require assets/js/google-map
-//= require assets/js/main
